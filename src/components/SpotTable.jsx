22f21d4f1b95d6ac042034989c1cb4f739ec4dd7
107060022 <ducklyu0301@gmail.com>
2021-04-22 23:33:18 +0800

    finish Scrolling

--- src/components/SpotTable.jsx
+++ src/components/SpotTable.jsx
@@ -4,86 +4,84 @@ import { Table } from "reactstrap";
 import { getSpotsFromMotc } from "api/motc-spot.js";
 
 export default class SpotTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lastCity: this.props.city,
-      skip: 0,
-      spots: [],
-      hasMore: true,
-    };
-  }
-  static getDerivedStateFromProps(props, state) {
-    if (props.city != state.lastCity) {
-      return {
-        lastCity: props.city,
-        skip: 0,
-      };
+    constructor(props) {
+        super(props);
+        this.state = {
+            skip: 0,
+            spots: [],
+            hasMore: true,
+        };
+        this.getSpots = this.getSpots.bind(this);
     }
-    return null;
-  }
-  componentDidMount() {
-    this.getSpots(this.props.city, this.state.skip);
-  }
 
-  render() {
-    // this.getSpots(this.props.city, this.state.skip);
-    let spotIdx = 0;
-    let tableItems = this.state.spots.map((spot) => (
-      <tr>
-        <th scope="row">{spotIdx++}</th>
-        <td>{spot.name}</td>
-        <td>{spot.description}</td>
-      </tr>
-    ));
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.city != prevProps.city) {
+            this.setState({ skip: 0 });
+        }
+    }
 
-    return (
-      <div className="container-fluid">
-        <InfiniteScroll
-          dataLength={this.state.spots.length}
-          next={this.getSpots}
-          hasMore={this.state.hasMore}
-          loader={<h4>Loading...</h4>}
-          height={600}
-          endMessage={
-            <p style={{ textAlign: "center" }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-        >
-          <Table striped>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>{tableItems}</tbody>
-          </Table>
-        </InfiniteScroll>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.getSpots(this.props.city, this.state.skip);
+    }
 
-  getSpots() {
-    if (this.state.hasMore === false) return;
-    this.setState(
-      {
-        skip: this.state.skip + 30,
-      },
-      () => {
-        getSpotsFromMotc(this.props.city, this.state.skip - 30)
-          .then((spots) => {
-            const newSpots = [...this.state.spots, ...spots];
-            this.setState({
-              spots: newSpots,
-            });
-          })
-          .catch((err) => {
-            this.setState({ hasMore: false });
-          });
-      }
-    );
-  }
+    render() {
+        let spotIdx = 0;
+        let tableItems = this.state.spots.map((spot) => (
+            <tr>
+                <th scope="row">{spotIdx++}</th>
+                <td>{spot.name}</td>
+                <td>{spot.description}</td>
+            </tr>
+        ));
+
+        return (
+            <div className="container-fluid">
+                <InfiniteScroll
+                    dataLength={this.state.spots.length}
+                    next={this.getSpots}
+                    hasMore={this.state.hasMore}
+                    loader={<h4>Loading...</h4>}
+                    height={600}
+                    endMessage={
+                        <p style={{ textAlign: "center" }}>
+                            <b>Yay! You have seen it all</b>
+                        </p>
+                    }
+                >
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>{tableItems}</tbody>
+                    </Table>
+                </InfiniteScroll>
+            </div>
+        );
+    }
+
+    getSpots() {
+        console.log(this.state.skip);
+        // if (this.state.hasMore === false) return;
+        this.setState(
+            (prevState, props) => ({
+                skip: prevState.skip + 30,
+            }),
+            () => {
+                getSpotsFromMotc(this.props.city, this.state.skip)
+                    .then((spots) => {
+                        let nextSpots = [...this.state.spots, ...spots];
+                        this.setState({
+                            spots: nextSpots,
+                        });
+                    })
+                    .catch((err) => {
+                        this.setState({ hasMore: false });
+                    });
+            }
+        );
+    }
 }
